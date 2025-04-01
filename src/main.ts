520d07d9832b708e2aed12cd311a5b8440abeb08
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2025-04-01 15:53:13 +0200

    màj selection + bouton transcrire

--- src/main.ts
+++ src/main.ts
@@ -18,14 +18,15 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>ALBERT</h1>
     <div class="card">
       <div id="file-drop-area" class="file-drop-area">
-        <p>Drag & drop your file here</p>
-        <p>or</p>
-        <button id="file-select-button">Select File</button>
-        <input type="file" id="file-input" accept="audio/wav, audio/mp3" hidden />
+        <p>Glissez / déposez<br> votre fichier audio ici</p>
+        <p>ou</p>
+        <button id="file-select-button">Sélectionnez un fichier</button>
       </div>
       <div id="file-path-display" class="file-path-display">
-        <p>No file selected</p>
+        <p>Aucun fichier sélectionné</p>
       </div>
+      <div>
+        <button id="file-submit-button" hidden>Transcrire</button>
     </div>
   </div>
 `
@@ -36,9 +37,34 @@ const fileSelectButton = document.getElementById('file-select-button') as HTMLBu
 const filePathDisplay = document.getElementById('file-path-display') as HTMLDivElement;
 
 // Handle file selection
-function handleFile(file: string) {
-  // Display the selected file path
-  filePathDisplay.innerHTML = `<p>Selected file: ${file}</p>`;
+function handleFile(filePath: string) {
+  // Get the file name from the path
+  const fileName = filePath.split('/').pop() || '';
+  const fileExtension = fileName.split('.').pop() || '';
+  const validExtensions = ['wav', 'mp3'];
+  let submitButton = document.getElementById('file-submit-button') as HTMLButtonElement;
+  // Hide the submit button by default  
+  submitButton.hidden = true;
+  let msg = '';
+  // Check if the file extension is valid
+  if (!validExtensions.includes(fileExtension)) {
+    msg = '<b>Type de fichier non accepté.<p>Sélectionnez svp un fichier audio<br> avec l\'extension .mp3 ou .wav</b>';
+  }
+  else {
+    msg =`<p>Fichier sélectionné :<br><b> ${fileName}</b></p>`;
+    // Show the submit button
+    submitButton.hidden = false;
+    // Add event listener to the submit button
+    submitButton.addEventListener('click', async () => {
+      // Here we would send the file path to Rust backend
+      // This will be implemented later
+      console.log('File submitted:', filePath);
+    }
+    );
+  }
+  filePathDisplay.innerHTML = msg;
+  
+
   
   // Here we would send the file path to Rust backend
   // This will be implemented later
@@ -73,19 +99,26 @@ const _unlisten = await getCurrentWebview().onDragDropEvent((event) => {
 // Traitement Tauri d'ouverture du selecteur de fichiers pour obtenir le path complet
 // Tauri file selector dialog
 // File button click event
+// https://tauri.app/reference/javascript/dialog/
 fileSelectButton.addEventListener('click', async (_event) => {
   // Open a dialog
   const file = await open({
     multiple: false,
     directory: false,
+    title: 'Selectionnez un fichier wav ou mp3',
+    filters: [
+      {
+        name: 'Audio Files',
+        extensions: ['wav', 'mp3'],
+      },
+    ],
   });
   if (file) {
     // Handle the selected file
     handleFile(file as string);
   }
 });
- // you need to call unlisten if your handler goes out of scope e.g. the component is unmounted
- //unlisten();
+
 
 
 

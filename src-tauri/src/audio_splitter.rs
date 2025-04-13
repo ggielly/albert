23165b44b7808828fa9d7cfb9c2cc7ef388feb8d
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2025-04-13 21:09:15 +0200

    essai language :  àtester

--- src-tauri/src/audio_splitter.rs
+++ src-tauri/src/audio_splitter.rs
@@ -1,10 +1,13 @@
-use mp3_splitter::minutes_to_duration;
+use mp3_splitter::minutes_to_duration as mp3_minutes_to_duration;
 use mp3_splitter::split_mp3;
-use mp3_splitter::SplitOptions;
+use mp3_splitter::SplitOptions as Mp3SplitOptions;
+use wav_splitter::SplitOptions as WavSplitOptions;
+use wav_splitter::minutes_to_duration as wav_minutes_to_duration;
+use wav_splitter::split_wav;
 use std::path::Path;
 
-/// Ce fichier contient la logique de découpage des fichiers audio mp3 en chunks.
-/// Il utilise la bibliothèque mp3_splitter pour effectuer le découpage.
+/// Ce fichier contient la logique de découpage des fichiers audio  en chunks.
+/// Il utilise les bibliothèques mp3_splitter et wav_splitter pour effectuer le découpage.
 /// Il stocke les fichiers chunks dans un répertoire spécifique dans le dossier de téléchargement de l'utilisateur.
 
 pub fn split_audio_file(
@@ -15,37 +18,83 @@ pub fn split_audio_file(
     // Get the user's download directory
     let output_dir = crate::get_chunk_directory()?;
     println!("Output directory: {}", output_dir.display());
-    let options = SplitOptions {
-        input_path: Path::new(file_path),
-        chunk_duration: minutes_to_duration(split_duration), // Convert minutes to Duration
-        output_dir: &output_dir,                             // Directory to save the chunks
-        prefix: session_name,                                // Prefix for output files
+    // Check if the file is a WAV or MP3 file
+    let file_extension = match Path::new(file_path).extension() {
+        Some(ext) => ext.to_str().unwrap_or(""),
+        None => "",
     };
-
-    // Perform the split
+    let file_extension = file_extension.to_lowercase();
+    let file_extension = file_extension.trim_start_matches('.');
+    println!("File extension: {}", file_extension);
+    // stockage des chemins des fichiers créés
     let mut chunk_paths: Vec<String> = vec![];
-    match split_mp3(&options) {
-        Ok(result) => {
-            println!("Split into {} chunks", result.chunk_count);
 
-            // Access information about the split
-            println!(
-                "Total duration: {:.2} minutes",
-                result.total_duration.as_secs_f64() / 60.0
-            );
+    // Determine the split options based on the file type
+    if file_extension == "mp3" {
+        let mp3_options = Mp3SplitOptions {
+            input_path: Path::new(file_path),
+            chunk_duration: mp3_minutes_to_duration(split_duration), // Convert minutes to Duration
+            output_dir: &output_dir,                                          // Directory to save the chunks
+            prefix: session_name,                                             // Prefix for output files
+        };
 
-            // You can also access all output file paths
-            for path in result.output_files {
-                chunk_paths.push(path.display().to_string());
-                println!("Created: {}", path.display());
+        // Perform the mp3 split
+        match split_mp3(&mp3_options) {
+            Ok(result) => {
+                println!("Split into {} chunks", result.chunk_count);
+
+                // Access information about the split
+                println!(
+                    "Total duration: {:.2} minutes",
+                    result.total_duration.as_secs_f64() / 60.0
+                );
+
+                // You can also access all output file paths
+                for path in result.output_files {
+                    chunk_paths.push(path.display().to_string());
+                    println!("Created: {}", path.display());
+                }
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                return Err(format!("Error splitting audio file: {}", e));
             }
         }
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            return Err(format!("Error splitting audio file: {}", e));
+    }
+    // sinon on considère que c'est du wav par défaut
+    else {
+        let wav_options = WavSplitOptions {
+            input_path: Path::new(file_path),
+            chunk_duration: wav_minutes_to_duration(split_duration), // Convert minutes to Duration
+            output_dir: &output_dir,                                          // Directory to save the chunks
+            prefix: session_name,                                              // Prefix for output files
+        };
+
+        // Perform the wav split
+        match split_wav(&wav_options) {
+            Ok(result) => {
+                println!("Split into {} chunks", result.chunk_count);
+
+                // Access information about the split
+                println!(
+                    "Total duration: {:.2} minutes",
+                    result.total_duration.as_secs_f64() / 60.0
+                );
+
+                // You can also access all output file paths
+                for path in result.output_files {
+                    chunk_paths.push(path.display().to_string());
+                    println!("Created: {}", path.display());
+                }
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                return Err(format!("Error splitting audio file: {}", e));
+            }
         }
     }
 
+    // On renvoie un OK avec la liste des fichiers créés
     Ok(chunk_paths)
 }
 
